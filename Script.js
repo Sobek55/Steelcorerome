dff056b09569b94b2e465d53240cb5da91f72ecf
Sobek55 <rnunez801@gmail.com>
2026-06-08 18:36:06 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,27 @@
+const customers = [];
+
+document.getElementById("customerForm").addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const customer = {
+
+        id: Date.now(),
+
+        name: document.getElementById("name").value,
+
+        email: document.getElementById("email").value,
+
+        password: document.getElementById("password").value,
+
+        favoriteGame: document.getElementById("favoriteGame").value
+
+    };
+
+    customers.push(customer);
+
+    console.clear();
+
+    console.log(customers);
+
+});
